449a127c442853f101527ee8fb1fa32d3599ad44
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2023-02-12 22:20:34 -0800

    Commit 1 (2023-02-12)
    - Made changes to classes to make them more obvious in terms of documentation.
    
    - Made a rough layout of user account log in/user wanting to create a new account.

--- Code/B.py
+++ Code/B.py
@@ -1,47 +1,72 @@
 import json
 
 
-class Class:
-    def __init__(self, name, block, file):
-        """ A class the user has and its attributes."""
+class Account:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        if password is None:
+            self.set_password()
+
+    def check_password(self, user_input) -> bool:
+        """ Checks to see if the user inputted their password correctly. Returns true if they did and false if they didn't. """
+        try:
+            if str(user_input) == str(self.password):
+                return True
+        except AttributeError:
+            if int(user_input) == int(self.password):
+                return True
+        else:
+            return False
+
+    def set_password(self):
+        """ Allows the user to set a password for their account. """
+        password = input("What would like your password to be?: ")
+        self.password = password
+
+
+class Subject:
+    def __init__(self, name, block, data_file):
+        """ Contains a subject/class's attributes."""
         self.name = name
         self.block = block
-        self.file = file
+        self.file = data_file
 
 
-class DataManagement:
+class JsonDataManagement:
     def __init__(self, file):
-        """ Stores and retrieves data stored in a json file. """
+        """ Stores and retrieves data stored in a given json file. """
         self.file = file
 
     def store_data(self, data):
-        """ Stores data in a file. """
+        """ Stores data in a json file. """
         with open(self.file, "w") as f:
             f.write(json.dumps(data))
 
     def get_data(self):
-        """ Returns data from a file. """
+        """ Retrieves data stored in a json file. """
         with open(self.file, "r") as f:
             content = f.read()
             return json.loads(content)
 
-    def to_json(self, data):
-        """ Converts data into a json accepted format. """
-        return data
 
-
-class TextEssentials:
+class Text:
     def __init__(self):
-        self.instructions = ['Press 1 to view your tasks.', 'Press 2 to add a task to your list.',
-                             'Press 3 to remove a task from your list.', 'Press 4 to exit the application.']
+        """ Contains all the text shown to the user while the application is running. """
+        self.menu_instructions = ['Press 1 to view your tasks.', 'Press 2 to add a task to your list.',
+                                  'Press 3 to remove a task from your list.', 'Press 4 to exit the application.']
 
-    def main_menu_text(self) -> int:
-        """ Returns the text used in the main menu. """
-        for text in self.instructions:
-            print(text)
+    def main_menu(self) -> int:
+        """ Runs the main menu and returns the choice the user made from the menu's options.  """
+        for text in self.menu_instructions:
+            print(text)  # Prints menu and options
         choice = input("What is your choice?: ")
+
         try:
-            return int(choice)
+            return int(choice)  # Returns the user's choice if they inputted an integer.
         except ValueError or AttributeError:
             print("Invalid Option, please try again!")
-            self.main_menu_text()
+            self.main_menu()
+
+
+
